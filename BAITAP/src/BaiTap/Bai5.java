5fb73c54594ce7d3d2b133e3c83df98a947383d1
Chuong2102 <39906259+Chuong2102@users.noreply.github.com>
2021-09-16 15:59:44 +0700

    new commit

---
+++ BAITAP/src/BaiTap/Bai5.java
@@ -0,0 +1,25 @@
+package BaiTap;
+import java.util.Scanner;
+public class Bai5 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner input = new Scanner(System.in);
+		
+		float a,b;
+		float result = 0;
+		System.out.println("Nhap a");
+		a = input.nextInt();
+		System.out.println("Nhap b");
+		b = input.nextInt();
+		//
+		if(a == 0) {
+			System.out.println("he so a phai khac 0");
+		}
+		else {
+			 result = -b/a;
+		}
+		System.out.println("Ket qua la`: " + result);
+	}
+
+}
